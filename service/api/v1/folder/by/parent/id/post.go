73f844427743e2b47011032328cb618c22fd2a1e
Anton Segerkvist <anton.stefan.roine.segerkvist@gmail.com>
2018-08-15 23:53:48 +0200

    Added comment to the post method.

--- service/api/v1/folder/by/parent/id/post.go
+++ service/api/v1/folder/by/parent/id/post.go
@@ -7,6 +7,9 @@ import (
 	"github.com/noteshare/session"
 )
 
+//
+// Post handles adding of a single folder to an other folder.
+//
 var Post = session.Authenticate(
 	func(w http.ResponseWriter, r *http.Request, _ httprouter.Params, s session.Session) {
 	},
